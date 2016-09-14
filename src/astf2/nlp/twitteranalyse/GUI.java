73095a12ef4fe92449bb08b8629be285d8929c03
Alistair Fisher <alistairstfisher@gmail.com>
2016-09-14 05:58:51 +0100

    Added Background to GUI

--- src/astf2/nlp/twitteranalyse/GUI.java
+++ src/astf2/nlp/twitteranalyse/GUI.java
@@ -71,6 +71,7 @@ public class GUI extends Application {
 
         Scene scene = new Scene(grid, 300, 275);
         primaryStage.setScene(scene);
+        scene.getStylesheets().add(GUI.class.getResource("GUI.css").toExternalForm());
         primaryStage.show();
     }
 }
